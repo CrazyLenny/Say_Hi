624477f8ff0c0447e85d263f79d1b024ac76ad34
CrazyLenny <ykxlenny@gmail.com>
2017-08-21 16:58:23 -0400

    Initial Checkin for Say Hi

---
+++ Say_Hi/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 21, 2017
+ *      Author: Kexin
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Say Hi! GIT Kexin!" << endl;
+}
+
+
